14cac26d94d96021142858b9a2e40d9f18215cb8
Kevin Orenday <varax297@gmail.com>
2021-03-21 23:30:45 -0300

    added selection of format and sorting functionality

--- js/main.js
+++ js/main.js
@@ -12,6 +12,8 @@ window.addEventListener('load', function() {
     let currentPage = 1;
     let lastPage;
     let search;
+    let format;
+    let sort;
 
     // Set the url endpoint for the API
     const url = 'https://graphql.anilist.co';
@@ -19,7 +21,7 @@ window.addEventListener('load', function() {
     // Set the query for the request
     const query =
     `
-        query ($page: Int, $search: String) {
+        query ($page: Int, $format: MediaFormat, $search: String, $sort: [MediaSort]) {
             Page (page: $page) {
                 pageInfo {
                     total
@@ -28,7 +30,7 @@ window.addEventListener('load', function() {
                     lastPage
                     hasNextPage
                 }
-                media (search: $search) {
+                media (format: $format, search: $search, sort: $sort) {
                     id
                     type
                     siteUrl
@@ -47,14 +49,15 @@ window.addEventListener('load', function() {
         }
     `;
 
+    // Initial call to load the first medias
+    requestAPI();
+
     // Add an event listener for searching media
     document.querySelector('#searchButton').addEventListener('click', function () {
-        if (searchBox.value !== null && searchBox.value !== undefined && searchBox.value.length !== 0) {   
+        if (searchBox.value !== null && searchBox.value !== undefined && searchBox.value.length !== 0)
             search = searchBox.value;
-        }
-        else {
-            search = null;
-        }
+        else
+            search = undefined;
         
         // Always set current page to ONE each search request
         currentPage = 1;
@@ -62,8 +65,76 @@ window.addEventListener('load', function() {
         requestAPI();
     });
 
-    // Initial call to load the first medias
-    requestAPI();
+    // Add a click event listener to each format buttons
+    for (let i = 0; i < document.querySelector('#formatList').children.length; i++) {
+        document.querySelector('#formatList').children[i]
+            .addEventListener('click', formatEventHandler);
+
+        function formatEventHandler(e) {
+            switch(e.target.innerText.toUpperCase()) {
+                case "TV":
+                    format = 'TV';
+                    break;
+                case "TV SHORT":
+                    format = 'TV_SHORT';
+                    break;
+                case "MOVIE":
+                    format = 'MOVIE'
+                    break;
+                case "SPECIAL":
+                    format = 'SPECIAL'
+                    break;
+                case "OVA":
+                    format = 'OVA'
+                    break;
+                case "ONA":
+                    format = 'ONA'
+                    break;
+                case "MUSIC":
+                    format = 'MUSIC'
+                    break;
+                case "MANGA":
+                    format = 'MANGA'
+                    break;
+                case "NOVEL":
+                    format = 'NOVEL'
+                    break;
+                case "ONE SHOT":
+                    format = 'ONE_SHOT'
+                    break;
+                default:
+                    format = undefined;
+                    break;
+            }
+            document.querySelector('#selectedFormat').innerText = e.target.innerText;
+            requestAPI();
+        }
+    }
+
+    // Add a click event listener to each sort by buttons
+    for (let i = 0; i < document.querySelector('#sortByList').children.length; i++) {
+        document.querySelector('#sortByList').children[i]
+            .addEventListener('click', sortEventHandler);
+
+        function sortEventHandler(e) {
+            switch(e.target.innerText.toUpperCase()) {
+                case "TITLE":
+                    sort = 'TITLE_ENGLISH_DESC';
+                    break;
+                case "POPULARITY":
+                    sort = 'POPULARITY_DESC';
+                    break;
+                case "TRENDING":
+                    sort = 'TRENDING_DESC'
+                    break;
+                default:
+                    sort = undefined;
+                    break;
+            }
+            document.querySelector('#selectedSort').innerText = e.target.innerText;
+            requestAPI();
+        }
+    }
 
     // Add a click event listener on each link in the pagination
     for (let i = 0; i < document.querySelector('#pageList').childElementCount; i++) {
@@ -231,7 +302,11 @@ window.addEventListener('load', function() {
         let variables = {
             page: currentPage,
             search: search,
+            sort: sort,
+            format: format,
         };
+
+        console.dir(variables);
 
         // Define the config for the API request
         let options = {
