957d41ee8a29af58c1b6f4efa5fcd07212f1ad65
Kevin Orenday <varax297@gmail.com>
2021-04-04 18:08:19 -0300

    updated the search query to add sort

--- js/Classes/Search.js
+++ js/Classes/Search.js
@@ -4,8 +4,10 @@ import WebAPI from './WebAPI.js';
 class Search {
     setEventListener() {
         document.querySelector('#searchButton').addEventListener('click', function () {
-            if (searchBox.value !== null && searchBox.value !== undefined && searchBox.value.length !== 0)
+            if (searchBox.value !== null && searchBox.value !== undefined && searchBox.value.length !== 0){
                 Global.search = searchBox.value;
+                Global.sort = 'SEARCH_MATCH';
+            }
             else
                 Global.search = undefined;
     
